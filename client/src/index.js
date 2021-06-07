e71d5debf205046182e7568da04313976a675791
fasikaWalle <fasikabini12@gmail.com>
2021-06-06 21:55:09 -0500

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import * as serviceWorker from "./serviceWorkers";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.register();
